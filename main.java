f1a913664cc8e86d37b1d542b80239575732cff0
Mateus Pereira <mateuspereiravazdasilva@gmail.com>
2026-02-25 12:09:05 -0300

    Primeiro commit

---
+++ main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+        System.out.println("Minha idade é: " + 29 + " anos.");
+        System.out.println(3+5<=21);
+    }
+}
