f29474ebb22841e767ee18cb18247bad859d624a
Mateus Pereira <mateuspereiravazdasilva@gmail.com>
2026-02-27 17:09:20 -0300

    Renomeando main.java para temp.java

--- main.java
+++
@@ -1,8 +0,0 @@
-// Teste push
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Olá, Mundo!");
-        System.out.println("Minha idade é: " + 29 + " anos.");
-        System.out.println(3+5<=21);
-    }
-}
